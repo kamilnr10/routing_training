d50de49e3d92cb4e935859f7822e50a2bf7bfeee
kamilnr10 <kamil.nr10@gmail.com>
2020-11-24 21:01:14 +0100

    Added some styles css

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navigation.css";
+
+const list = [
+  { name: "start", path: "/", exact: true },
+  { name: "produkty", path: "/products" },
+  { name: "kontakt", path: "/contact" },
+  { name: "panel admina", path: "/admin" },
+];
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const menu = list.map((item) => (
+    <li key={item.name}>
+      <NavLink to={item.path} exact={item.exact ? item.exact : false}>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+
+  return (
+    <div>
+      <nav className="main">
+        <ul>{menu}</ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
